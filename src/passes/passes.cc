ba08ceab0c8c70fcf900dad0bb2c85e24fd05737
Richard Widén <richard.widen0161@student.uu.se>
2025-06-04 13:41:19 +0200

    Return error for programs containing polymorph types

--- src/passes/passes.cc
+++ src/passes/passes.cc
@@ -16,14 +16,18 @@
 #include "typecheck/solve_constraints.cc"
 #include "typecheck/resolve_print.cc"
 
+#include "llvm-ir/resolve_polymorphism.cc"
+
 #include "llvm-ir/free_variables.cc"
 #include "llvm-ir/propagate_free_variables.cc"
 #include "llvm-ir/globals.cc"
 #include "llvm-ir/main_function.cc"
 #include "llvm-ir/closure_conversion.cc"
 #include "llvm-ir/closure_globals.cc"
+
 #include "llvm-ir/compile.cc"
 #include "llvm-ir/blockify.cc"
+
 #include "llvm-ir/code_generation.cc"
 // clang-format on
 
@@ -56,6 +60,9 @@ namespace miniml {
       cleanup_constraints(),
       resolve_print(),
 
+      // TODO: Polymorphism not supported
+      resolve_polymorphism(),
+
       // Generate IR
       free_variables(),
       propagate_free_variables(),
@@ -68,7 +75,7 @@ namespace miniml {
       compile(),
       blockify(),
 
-      // // Code generation
+      // Code generation
       llvmir::code_generation(),
     };
   }
